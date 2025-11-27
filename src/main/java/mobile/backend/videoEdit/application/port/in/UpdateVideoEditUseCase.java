4b70e607fdd9a9b20185b27a3fc0d0311eda989f
jucheolkang <jucheol9877@naver.com>
2025-11-27 13:34:25 +0900

    ♻️ Refactor : useCase 통합

--- src/main/java/mobile/backend/videoEdit/application/port/in/UpdateVideoEditUseCase.java
+++
@@ -1,8 +0,0 @@
-package mobile.backend.videoEdit.application.port.in;
-
-import mobile.backend.videoEdit.domain.command.UpdateVideoEditCommand;
-import mobile.backend.videoEdit.domain.model.VideoEdit;
-
-public interface UpdateVideoEditUseCase {
-    VideoEdit updateThumbnail(UpdateVideoEditCommand command);
-}
