8e626536def1f9f7d00d0c4215ad4972c1ffdab7
bikooju <jhjk1234@gmail.com>
2025-12-05 23:23:18 +0900

    :recycle: userId에서 CustomUserDetails로 변경

--- src/main/java/mobile/backend/notice/adapter/in/web/NoticeController.java
+++ src/main/java/mobile/backend/notice/adapter/in/web/NoticeController.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import mobile.backend.global.adapter.in.web.response.BaseResponse;
 import mobile.backend.global.adapter.in.web.response.SimplePageResponse;
+import mobile.backend.global.security.CustomUserDetails;
 import mobile.backend.notice.adapter.in.web.response.NoticeResponse;
 import mobile.backend.notice.application.port.in.NoticeCommandUseCase;
 import mobile.backend.notice.application.port.in.NoticeQueryUseCase;
 import mobile.backend.notice.domain.model.Notice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,9 +47,9 @@ public class NoticeController {
   }
 
   @Operation(summary = "홈 화면 최신 공지 1건 조회", description = "홈 화면 진입 시 보여줄 최신 공지 1건을 조회합니다. 사용자가 다시보지 않기를 누른 공지는 반환하지 않습니다.")
-  @GetMapping("/home/{userId}")
-  public BaseResponse<NoticeResponse> getHomeNotice(@PathVariable Long userId) { // TODO: JWT 쓰면 @AuthenticationPrincipal로 교체
-    Notice notice = noticeQueryUseCase.getHomeNotice(userId);
+  @GetMapping("/home")
+  public BaseResponse<NoticeResponse> getHomeNotice(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    Notice notice = noticeQueryUseCase.getHomeNotice(userDetails.getUserId());
 
     if (notice == null) {
       return BaseResponse.success("현재 볼 수 있는 최신 공지가 없습니다.", null);
@@ -57,12 +59,12 @@ public class NoticeController {
   }
 
   @Operation(summary = "공지 다시보지 않기", description = "특정 공지에 대해 다시보지 않기(ignored)를 설정합니다. 토큰 필수")
-  @PostMapping("/{noticeId}/{userId}/ignore")
+  @PostMapping("/{noticeId}/ignore")
   public BaseResponse<Void> ignoreNotice(
-      @PathVariable Long userId,
+      @AuthenticationPrincipal CustomUserDetails userDetails,
       @PathVariable Long noticeId
   ) {
-    noticeCommandUseCase.ignoreNotice(userId, noticeId);
+    noticeCommandUseCase.ignoreNotice(userDetails.getUserId(), noticeId);
     String message = String.format("공지 %d번을 다시보지 않기로 설정했습니다.", noticeId);
     return BaseResponse.success(message, null);
   }
