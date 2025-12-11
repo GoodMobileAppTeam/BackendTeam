48b641a2a6c19568cf3adc327ab9e5997b338e97
jucheolkang <jucheol9877@naver.com>
2025-12-12 01:17:19 +0900

    🐛 Fix : UserDetails -> CustomUserDetails로 수정

--- src/main/java/mobile/backend/videoEdit/adapter/in/web/VideoEditController.java
+++ src/main/java/mobile/backend/videoEdit/adapter/in/web/VideoEditController.java
@@ -47,6 +47,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.List;
+
 @Tag(name = "Video Edit", description = "영상 편집 관리 API")
 @RestController
 @RequestMapping("/v1/videos")
