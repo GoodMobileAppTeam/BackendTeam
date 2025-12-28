13a9133e0c98e1da04605ba5c319d646ff4764b9
jucheolkang <jucheol9877@naver.com>
2025-12-28 21:13:24 +0900

    ♻️ Refactor : 반환값을 CursorPageResult로 수정

--- src/main/java/mobile/backend/videoEdit/application/port/in/VideoEditQueryUseCase.java
+++ src/main/java/mobile/backend/videoEdit/application/port/in/VideoEditQueryUseCase.java
@@ -1,5 +1,6 @@
 package mobile.backend.videoEdit.application.port.in;
 
+import mobile.backend.videoEdit.application.service.CursorPageResult;
 import mobile.backend.videoEdit.domain.command.SearchSummaryVideoEditCommand;
 import mobile.backend.videoEdit.domain.command.SearchVideoEditCommand;
 import mobile.backend.videoEdit.domain.model.VideoEditSummary;
@@ -9,6 +10,6 @@ import java.util.List;
 
 public interface VideoEditQueryUseCase {
     VideoEdit getById(Long id, Long userId);
-    List<VideoEdit> search(SearchVideoEditCommand command);
+    CursorPageResult<VideoEdit> search(SearchVideoEditCommand command);
     List<VideoEditSummary> getDailySummary(SearchSummaryVideoEditCommand criteria);
 }
