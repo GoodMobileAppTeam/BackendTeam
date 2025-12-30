8b2bd82d617022f1c65ffa07284fb0caf23a7d6a
jucheolkang <jucheol9877@naver.com>
2025-12-30 16:13:50 +0900

    ♻️ Refactor : import 추가

--- src/main/java/mobile/backend/videoEdit/adapter/in/web/VideoEditController.java
+++ src/main/java/mobile/backend/videoEdit/adapter/in/web/VideoEditController.java
@@ -11,6 +11,7 @@ import mobile.backend.global.security.CustomUserDetails;
 import mobile.backend.videoEdit.adapter.in.web.request.CreateVideoEditRequest;
 import mobile.backend.videoEdit.adapter.in.web.request.VideoEditSearchRequest;
 import mobile.backend.videoEdit.adapter.in.web.request.VideoEditSummaryRequest;
+import mobile.backend.videoEdit.adapter.in.web.request.place.PlaceNameListRequest;
 import mobile.backend.videoEdit.adapter.in.web.response.VideoEditSearchResponse;
 import mobile.backend.videoEdit.adapter.in.web.request.bgm.SetVideoEditBgmRequest;
 import mobile.backend.videoEdit.adapter.in.web.request.place.PlaceNameSearchRequest;
