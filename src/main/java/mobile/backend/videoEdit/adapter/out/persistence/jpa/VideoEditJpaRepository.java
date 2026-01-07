2178100d56b4cf2a0a42827630c2609b3ecf8147
jucheolkang <jucheol9877@naver.com>
2026-01-07 15:25:00 +0900

    ♻️ Refactor : 쿼리 수정

--- src/main/java/mobile/backend/videoEdit/adapter/out/persistence/jpa/VideoEditJpaRepository.java
+++ src/main/java/mobile/backend/videoEdit/adapter/out/persistence/jpa/VideoEditJpaRepository.java
@@ -10,29 +10,27 @@ import java.util.List;
 
 public interface VideoEditJpaRepository extends JpaRepository<VideoEditEntity, Long> {
     @Query(value = """
-    SELECT 
-        v.userDefinedDate AS userDefinedDate,
-        v.thumbnail_url AS latestThumbnail,
-        t.cnt AS count
-    FROM video_edit v
-    INNER JOIN (
         SELECT 
-            user_defined_date,
-            COUNT(*) AS cnt,
-            MAX(created_at) AS latestCreatedAt
-        FROM video_edit
-        WHERE user_id = :userId
-          AND user_defined_date BETWEEN :startDate AND :endDate
-        GROUP BY user_defined_date
-    ) t
-        ON v.user_defined_date = t.user_defined_date
-       AND v.created_at = t.latestCreatedAt
-    WHERE v.user_id = :userId
-      AND v.user_defined_date BETWEEN :startDate AND :endDate
-    ORDER BY v.user_defined_date DESC
-    """,
-            nativeQuery = true
-    )
+            v.user_defined_date AS userDefinedDate,
+            v.thumbnail_url AS latestThumbnail,
+            t.cnt AS count
+        FROM video_edit v
+        INNER JOIN (
+            SELECT 
+                user_defined_date,
+                COUNT(*) AS cnt,
+                MAX(created_at) AS latestCreatedAt
+            FROM video_edit
+            WHERE user_id = :userId
+              AND user_defined_date BETWEEN :startDate AND :endDate
+            GROUP BY user_defined_date
+        ) t
+            ON v.user_defined_date = t.user_defined_date
+           AND v.created_at = t.latestCreatedAt
+        WHERE v.user_id = :userId
+          AND v.user_defined_date BETWEEN :startDate AND :endDate
+        ORDER BY v.user_defined_date DESC
+        """, nativeQuery = true)
     List<VideoDailySummaryProjection> findDailySummary(
             @Param("userId") Long userId,
             @Param("startDate") LocalDate startDate,
